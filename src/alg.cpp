52db6c02e622ce58514e43af1407bfcfc0f46f6f
MikhailTumashuk <62620293+HuckmyLife@users.noreply.github.com>
2020-05-08 22:59:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -98,8 +98,8 @@ int eval(string pst)
             stack.pop();
 
 } 
-            int result = calculate(num2, num1, ch);
-            stack.push(result);
+            int res = calculate(num2, num1, ch);
+            stack.push(res);
         }
 
     }
